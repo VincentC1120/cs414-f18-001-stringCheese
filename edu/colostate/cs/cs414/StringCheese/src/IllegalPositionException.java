1464a39f3822468b46b0b15e5ed99538632a6f43
Chris Schaffer <c_schaffer4@yahoo.com>
2018-10-26 01:05:54 -0600

    Update IllegalPositionException.java

--- edu/colostate/cs/cs414/StringCheese/src/IllegalPositionException.java
+++ edu/colostate/cs/cs414/StringCheese/src/IllegalPositionException.java
@@ -1,5 +1,7 @@
 package edu.colostate.cs.cs414.StringCheese.src;
 
 public class IllegalPositionException extends Exception{
-
+    public IllegalPositionException(String message){
+        super(message);
+    }
 }
