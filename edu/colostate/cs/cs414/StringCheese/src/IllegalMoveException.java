f9a17096f476dc7857286b179608cb50d45330c7
Chris Schaffer <c_schaffer4@yahoo.com>
2018-10-26 15:18:23 -0600

    Update IllegalMoveException.java

--- edu/colostate/cs/cs414/StringCheese/src/IllegalMoveException.java
+++ edu/colostate/cs/cs414/StringCheese/src/IllegalMoveException.java
@@ -1,5 +1,7 @@
 package edu.colostate.cs.cs414.StringCheese.src;
 
 public class IllegalMoveException extends Exception{
-
+    public IllegalMoveException(String message){
+        super(message);
+    }
 }
